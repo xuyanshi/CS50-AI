b4ca291d7bb1df47a92874a77e1b44c5ce7a272e
Yanshi XU <xuyanshi1999@gmail.com>
2023-04-26 16:54:26 +0800

    mark

--- Project1/minesweeper/minesweeper.py
+++ Project1/minesweeper/minesweeper.py
@@ -122,14 +122,17 @@ class Sentence:
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
+            self.count -= 1
 
     def mark_safe(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be safe.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
 
 
 class MinesweeperAI:
