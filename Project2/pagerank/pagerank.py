3cbef01bb59cbdc823c61ade9500cd424081d7d2
Yanshi XU <xuyanshi1999@gmail.com>
2023-05-04 14:49:30 +0800

    Update pagerank.py

--- Project2/pagerank/pagerank.py
+++ Project2/pagerank/pagerank.py
@@ -54,7 +54,8 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    
+    for start_link, end_links in corpus:
+        pass
     return {}
 
 
