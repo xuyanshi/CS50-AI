f1c2da06e5c5fc812731f48214d75401c4362c44
Yanshi XU <xuyanshi1999@gmail.com>
2023-05-04 15:06:09 +0800

    update test

--- Project2/pagerank/pagerank_test.py
+++ Project2/pagerank/pagerank_test.py
@@ -1,1 +1,17 @@
 from pagerank import *
+
+
+def transition_model_test():
+    """
+    For example, if the corpus were {"1.html": {"2.html", "3.html"}, "2.html": {"3.html"}, "3.html": {"2.html"}},
+     the page was "1.html", and the damping_factor was 0.85,
+      then the output of transition_model should be {"1.html": 0.05, "2.html": 0.475, "3.html": 0.475}.
+    """
+    corpus = {"1.html": {"2.html", "3.html"}, "2.html": {"3.html"}, "3.html": {"2.html"}}
+    page = "1.html"
+    damping_factor = 0.85
+
+    print(transition_model(corpus, page, damping_factor))
+
+
+transition_model_test()
