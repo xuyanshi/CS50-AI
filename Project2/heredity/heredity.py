2ff2cea1fbae2d38421c8fbe9c3ca1c1d4415cba
Yanshi XU <xuyanshi1999@gmail.com>
2023-05-06 13:19:32 +0800

    heredity all finished

--- Project2/heredity/heredity.py
+++ Project2/heredity/heredity.py
@@ -203,7 +203,18 @@ def update(probabilities, one_gene, two_genes, have_trait, p):
     Which value for each distribution is updated depends on whether
     the person is in `have_gene` and `have_trait`, respectively.
     """
-    raise NotImplementedError
+    for person in probabilities:
+        if person in two_genes:
+            probabilities[person]["gene"][2] += p
+        elif person in one_gene:
+            probabilities[person]["gene"][1] += p
+        else:
+            probabilities[person]["gene"][0] += p
+
+        if person in have_trait:
+            probabilities[person]["trait"][True] += p
+        else:
+            probabilities[person]["trait"][False] += p
 
 
 def normalize(probabilities):
