0c78c3284054ee6fb4e2aa3a17918a862b4cb66c
Yanshi XU <xuyanshi1999@gmail.com>
2023-05-06 13:08:55 +0800

    Update heredity_test.py

--- Project2/heredity/heredity_test.py
+++ Project2/heredity/heredity_test.py
@@ -2,7 +2,11 @@ from heredity import *
 
 
 def joint_probability_test():
-    pass
-
+    people = {
+        'Harry': {'name': 'Harry', 'mother': 'Lily', 'father': 'James', 'trait': None},
+        'James': {'name': 'James', 'mother': None, 'father': None, 'trait': True},
+        'Lily': {'name': 'Lily', 'mother': None, 'father': None, 'trait': False}
+    }
+    
 
 print(joint_probability_test())
