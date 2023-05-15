27c553786d2b79fcd22bf418bcdfc709f7a5867a
Yanshi XU <xuyanshi1999@gmail.com>
2023-05-15 09:49:55 +0800

    update

--- Project4/shopping/shopping.py
+++ Project4/shopping/shopping.py
@@ -61,7 +61,7 @@ def load_data(filename):
     evidence = []
     labels = []
     with open(filename) as file:
-        ...
+        reader = csv.reader(file)
 
 
 def train_model(evidence, labels):
