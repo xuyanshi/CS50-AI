722c4bb074768fb97165fad6a0a38c6add83f9dc
Yanshi XU <xuyanshi1999@gmail.com>
2023-05-15 10:34:12 +0800

    Update shopping.py

--- Project4/shopping/shopping.py
+++ Project4/shopping/shopping.py
@@ -112,7 +112,9 @@ def evaluate(labels, predictions):
     representing the "true negative rate": the proportion of
     actual negative labels that were accurately identified.
     """
-    raise NotImplementedError
+    sensitivity = specificity = 0.0
+    
+    return sensitivity, specificity
 
 
 if __name__ == "__main__":
