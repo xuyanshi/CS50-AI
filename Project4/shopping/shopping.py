e9de4d3ec1b8c4ca448bd3cb25f5c5035fc4148c
Yanshi XU <xuyanshi1999@gmail.com>
2023-05-15 09:56:25 +0800

    Update shopping.py

--- Project4/shopping/shopping.py
+++ Project4/shopping/shopping.py
@@ -60,6 +60,8 @@ def load_data(filename):
     """
     evidence = []
     labels = []
+    months = {'Jan': 0, 'Feb': 1, 'Mar': 2, 'Apr': 3, 'May': 4, 'June': 5, 'Jul': 6, 'Aug': 7, 'Sep': 8, 'Oct': 9,
+              'Nov': 10, 'Dec': 11}
     with open(filename) as file:
         reader = csv.reader(file)
 
