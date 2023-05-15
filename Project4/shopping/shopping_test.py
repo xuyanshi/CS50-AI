cba4a7041e3fa5158f44d541d5bff04708171c9e
Yanshi XU <xuyanshi1999@gmail.com>
2023-05-15 09:51:23 +0800

    add test

---
+++ Project4/shopping/shopping_test.py
@@ -0,0 +1,12 @@
+import unittest
+from shopping import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        # self.assertEqual(True, False)  # add assertion here
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
