e133e7d5de0a9a6b1af4ab41c5bd6d93d0fc8ec0
Yanshi XU <xuyanshi1999@gmail.com>
2023-05-15 15:48:09 +0800

    Update nim.py

--- Project4/nim/nim.py
+++ Project4/nim/nim.py
@@ -118,7 +118,7 @@ class NimAI():
         `alpha` is the learning rate, and `new value estimate`
         is the sum of the current reward and estimated future rewards.
         """
-        self.q[state, action] = old_q + self.alpha * (sum(reward, future_rewards) - old_q)
+        self.q[state, action] = old_q + self.alpha * (reward + future_rewards - old_q)
 
     def best_future_reward(self, state):
         """
