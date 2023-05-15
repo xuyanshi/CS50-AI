ac6349a1ba1ac3f8550b1bf54192a0925cb7424e
Yanshi XU <xuyanshi1999@gmail.com>
2023-05-15 10:20:11 +0800

    Update shopping_test.py

--- Project4/shopping/shopping_test.py
+++ Project4/shopping/shopping_test.py
@@ -1,7 +1,7 @@
 from shopping import *
 
-data = load_data("shopping.csv")
-
-print(data[0][:10])
-print(data[1][:10])
-print(len(data[0]), len(data[1]))
+if __name__ == '__main__':
+    data = load_data("shopping.csv")
+    print(data[0][:10])
+    print(data[1][:10])
+    print(len(data[0]), len(data[1]))
