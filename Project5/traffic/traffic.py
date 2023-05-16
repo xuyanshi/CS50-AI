84e1944cda81922f6e4d1df1f121bee7e28122c9
Yanshi XU <xuyanshi1999@gmail.com>
2023-05-17 03:49:11 +0800

    Update traffic.py

--- Project5/traffic/traffic.py
+++ Project5/traffic/traffic.py
@@ -75,8 +75,36 @@ def get_model():
     Returns a compiled convolutional neural network model. Assume that the
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
+    Reference: https://github.com/languephone/AI50-Traffic/blob/main/traffic.py
     """
-    model = tf.keras.models.Sequential([])
+    model = tf.keras.models.Sequential([
+        # Convolutional layer. Learn 32 filters using a 3x3 kernel
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="relu",
+            input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+
+        # Max-pooling layer, using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        # Second convolutional layer. Learn 32 filters using a 3x3 kernel
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="relu"
+        ),
+
+        # Second pooling layer, using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        # Flatten units
+        tf.keras.layers.Flatten(),
+
+        # Add a hidden layer with dropout
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.2),
+
+        # Add an output layer with output units for all 43 signs
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])
 
     model.compile(
         optimizer="adam",
