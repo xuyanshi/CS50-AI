10d3a43d688063678a815180b2d331bfc6da2e10
Yanshi XU <xuyanshi1999@gmail.com>
2023-05-18 09:20:09 +0800

    Update parser.py

--- Project6/parser/parser.py
+++ Project6/parser/parser.py
@@ -16,6 +16,7 @@ V -> "smiled" | "tell" | "were"
 
 NONTERMINALS = """
 S -> N V
+NP -> N | Det N
 """
 
 grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
@@ -61,7 +62,7 @@ def preprocess(sentence):
     and removing any word that does not contain at least one alphabetic
     character.
     """
-    
+
     raise NotImplementedError
 
 
