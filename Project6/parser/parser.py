d21943c30a1ca8b8971cdc2ebe34472d1b5f4548
Yanshi XU <xuyanshi1999@gmail.com>
2023-05-18 10:49:01 +0800

    Update parser.py

--- Project6/parser/parser.py
+++ Project6/parser/parser.py
@@ -79,7 +79,9 @@ def np_chunk(tree):
     whose label is "NP" that does not itself contain any other
     noun phrases as subtrees.
     """
-    raise NotImplementedError
+    ans = []
+    
+    return ans
 
 
 if __name__ == "__main__":
